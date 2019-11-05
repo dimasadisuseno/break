0013fccfb03b9e9a4c32468a37f6c2e227c2d6ca
dimasadisuseno <57306195+dimasadisuseno@users.noreply.github.com>
2019-11-05 09:25:52 +0700

    Add files via upload

---
+++ Dimasadi.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dimasadi;
+
+/**
+ *
+ * @author dimaspc
+ */
+public class Dimasadi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int hari = 5;
+      switch (hari) {
+          case 1:
+          System.out.println("senin");
+          break;
+          case 2:
+          System.out.println("selasa");
+          break;
+          case 3:
+          System.out.println("rabu");
+          break;
+          case 4:
+          System.out.println("kamis");
+          break;
+          case 5:
+          System.out.println("jumat");
+          break;
+          case 6:
+          System.out.println("sabtu");
+          break;
+          case 7:
+          System.out.println("minggu");
+          break;
+      }
+    }
+}
+{
+        int hari = 0;
+        while (hari < 5){
+        System.out.println(nilai);
+        }
+    }
+    
+}
